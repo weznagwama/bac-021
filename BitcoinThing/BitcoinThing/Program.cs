37eac560e6ba764005a68a9d4a3ae91302caa384
Tristan Lymbery <tristan.lymbery@dominos.com.au>
2017-10-06 11:23:09 +1000

    BC thing.

---
+++ BitcoinThing/BitcoinThing/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NBitcoin;
+
+namespace BitcoinThing {
+    class Program {
+        static void Main(string[] args)
+        {
+
+            Key privateKey = new Key();
+            PubKey publicKey = privateKey.PubKey;
+            Console.WriteLine("Our key is {0}",publicKey);
+            Console.ReadLine();
+        }
+    }
+}
